622eb61a3274e8b81097bf6708dba41ee432a587
George-Chou <george_chou@gss.com.tw>
2022-08-25 10:59:08 +0800

    透過 Reentrant Lock 處理同步[無法解決]

--- src/main/java/org/dream/jpa/transaction/service/PostService.java
+++ src/main/java/org/dream/jpa/transaction/service/PostService.java
@@ -1,6 +1,8 @@
 package org.dream.jpa.transaction.service;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.dream.jpa.transaction.model.Post;
 import org.dream.jpa.transaction.repository.PostRepository;
@@ -15,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 public class PostService {
 
     private PostRepository postRepository;
+    private final ReentrantLock lock = new ReentrantLock();
     
     @Autowired
     public void setPostRepository(PostRepository postRepository) {
@@ -27,16 +30,33 @@ public class PostService {
     }
 
     @Transactional
-    public synchronized void findPostAndUpdateContent(Integer postId, String newContent) {
-        Optional<Post> postOptional = this.postRepository.findById(postId);
-        if(postOptional.isPresent()) {
-            // 這段邏輯假設取出的資料內容是存成字串的 int
-            Post post = postOptional.get();
-            String oldContent = post.getContent();
-            int oldValue = Integer.parseInt(oldContent);
-            int newValue = oldValue + 1;
-            log.info("舊值: {}, 新值: {}", oldValue, newValue);
-            post.setContent(Integer.toString(newValue));
+    public void findPostAndUpdateContent(Integer postId, String newContent) {
+        try {
+            /**
+             * 因為 Service 是 Singleton，所以每個 Thread 會共用同一個 ReentrantLock instance
+             * 這段設定的目的跟 "synchronized" 是一致的，但細節不同
+             */
+            boolean acquired = lock.tryLock(2000, TimeUnit.SECONDS);
+            if(acquired) {
+                Optional<Post> postOptional = this.postRepository.findById(postId);
+                if(postOptional.isPresent()) {
+                    // 這段邏輯假設取出的資料內容是存成字串的 int
+                    Post post = postOptional.get();
+                    String oldContent = post.getContent();
+                    int oldValue = Integer.parseInt(oldContent);
+                    int newValue = oldValue + 1;
+                    log.info("舊值: {}, 新值: {}", oldValue, newValue);
+                    post.setContent(Integer.toString(newValue));
+                }
+            }
+        }
+        catch(InterruptedException ex) {
+            log.error("取得 lock 時發生錯誤", ex);
+        }
+        finally {
+            if(lock.isLocked()) {
+                lock.unlock();
+            }
         }
     }
 
